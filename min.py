089636134611a6d1931d8bf855b87c95c4b458b9
Konstantin Fessaliysky <kfessalijskij@gmail.com>
2024-12-26 16:53:13 +0300

    changed_to_minimum

--- min.py
+++ min.py
@@ -1,7 +1,7 @@
 def lesson2(numbers):
-    m = -10**12
+    m = 10**12
     for i in range(len(numbers)):
-        if numbers[i]>m:
+        if numbers[i]<m:
             m = numbers[i]
     return m
 
